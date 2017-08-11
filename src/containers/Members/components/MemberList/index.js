5fc38433fbef4b48b978d623b9d05374892e6e32
Mike Eastes <michael.eastes@gmail.com>
2017-08-10 19:24:17 -0500

    Implements `MemberList`. * Adds mock data for display `MemberList`.

--- src/containers/Members/components/MemberList/index.js
+++ src/containers/Members/components/MemberList/index.js
@@ -1,3 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default () => <div>Member List</div>;
+import MaterialList, { ListItem, ListItemText } from 'material-ui/List';
+
+const List = styled(MaterialList)`
+  width: 100%;
+  max-width: 300px;
+  /* 16px is the padding on the material-ui List */
+  height: calc(100vh - 16px);
+  overflow: auto;
+  background-color: white;
+`;
+
+const MemberList = (props) => {
+  const { members } = props;
+
+  const items = members.map(member =>
+    (<ListItem key={member.handle} button>
+      <ListItemText primary={member.name} secondary={member.handle} />
+    </ListItem>),
+  );
+
+  if (members && members.length) {
+    return (
+      <List>
+        {items}
+      </List>
+    );
+  }
+  return null;
+};
+
+MemberList.defaultProps = {
+  members: [],
+};
+
+export default MemberList;
--- src/containers/Members/index.js
+++ src/containers/Members/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
 import { Route } from 'react-router-dom';
+
+import MemberList from './components/MemberList';
 
 import MemberDetail from './containers/MemberDetail';
 
+const mockMembers = [
+  { name: 'John Smith', handle: 'johnsmith1' },
+  { name: 'Jane Doe', handle: 'calculon' },
+  { name: 'Eric Johnson', handle: 'dude1983' },
+];
+
 export default () =>
   (<div>
-    <p>Members</p>
+    <MemberList members={mockMembers} />
     <Route path="/members/:id" component={MemberDetail} />
   </div>);
